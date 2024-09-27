ae7be9b22ed3b59baad29cefa8f414192cbe21ff
Tushar-Hossain <tusharhossain455@gmail.com>
2024-09-28 00:58:35 +0600

    local module exports

---
+++ 1_local_module_exports_require/index.js
@@ -0,0 +1,7 @@
+const { getName } = require("./student");
+
+console.log(getName());
+
+// console.log(s1.getName());
+// console.log(s1.getAge());
+// console.log(s1.cgpa);
---
+++ 1_local_module_exports_require/student.js
@@ -0,0 +1,22 @@
+exports.getName = () => {
+  return "Tushar";
+};
+// const getName = () => {
+//   return "Tushar";
+// };
+
+const getAge = () => {
+  return 22;
+};
+
+const cgpa = 3.22;
+
+// module.exports = {
+//   getName,
+//   getAge,
+//   cgpa,
+// };
+
+// exports.getName = getName;
+// exports.getAge = getAge;
+// exports.cgpa = cgpa;
