49f094023467f2f0f634cfc69018b595f94bf973
Tushar-Hossain <tusharhossain455@gmail.com>
2024-09-29 16:13:39 +0600

    os and path nodejs module

---
+++ 3_os_and_path_module/index.js
@@ -0,0 +1,15 @@
+// os and path module
+
+const os = require("os");
+
+// console.log(os.homedir());
+// console.log(os.hostname());
+// console.log(os.freemem());
+// console.log(os.totalmem());
+// console.log(os.userInfo());
+
+const path = require("path");
+
+// console.log(path.extname("index.html"));
+// console.log(path.join(__dirname + "/views"));
+console.log(path.join(__dirname + "/../views"));
